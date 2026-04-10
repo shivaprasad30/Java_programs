d9b0ce65d87945cbdfa00cb35ecef477fc530436
unknown <savulashivaprasad_cse225a0506@mgit.ac.in>
2026-04-10 12:07:15 +0530

    10-04-26 2

--- Fib_WO_AO.java
+++ Fib_WO_AO.java
@@ -1,3 +1,29 @@
-public class Fib_WO_AO {
+import java.util.*;
+class Fib_WO_AO {
 
+    //Fibonacci series without using arithmetic operators
+    static int add(int a, int b) {
+        while (b != 0) {
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int a = 0, b = 1;
+
+        System.out.print(a + " " + b + " ");
+
+        for (int i = 2; i < n; i++) {
+            int c = add(a, b); // no + used
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
 }
